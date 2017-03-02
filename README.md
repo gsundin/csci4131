a0c8e1aa316270c1c1695bf93cac2b0d958d1a08
unknown <sundi024@ECE-IEEE05.ad.umn.edu>
2017-03-02 16:46:47 -0600

    add: Migrate repo to personal account

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # csci4131
-For homework in CSCI4131 (Internet Programming) at UMN
+Homework and projects for Internet Programming
---
+++ hw4/javascript.js
@@ -0,0 +1,10 @@
+document.writeln("<div class=\"violet2 box\"><i>Violet</i></div>");
+document.writeln("<div class=\"indigo2 box\"><i>Indigo</i></div>");
+document.writeln("<div class=\"blue2 box\"><i>Blue</i></div>");
+document.writeln("<div class=\"green2 box\"><i>Green</i></div>");
+document.writeln("<div class=\"yellow2 box\"><i>Yellow</i></div>");
+document.writeln("<div class=\"orange2 box\"><i>Orange</i></div>");
+document.writeln("<div class=\"red2 box\"><i>Red</i></div>");
+document.writeln("<div class=\"black2 box\"><i>This is not a color of the rainbow.</i></div>");
+
+//alert("script finished");
