7482168a3e173f2a2841b6bd11ba2fbbc581fb1c
Garrett Sundin <sundi024@umn.edu>
2017-04-13 23:15:40 -0500

    add: Base files for hw8

---
+++ hw8/my_parse_file.php
@@ -0,0 +1,12 @@
+<?php
+
+	echo '{ FirstName: ' . $_POST['firstname'] . ',' . "<br>"
+	. '   LastName: ' . $_POST['lastname'] . ',' . "<br>"
+	. '   Address Line 1: ' . $_POST['add1'] . ',' . "<br>"
+	. '   Address Line 2: ' . $_POST['add2'] . ',' . "<br>"
+	. '   City: ' . $_POST['city'] . ',' . "<br>"
+	. '   State: ' . $_POST['state'] . ',' . "<br>"
+	. '   Zip: ' . $_POST['zip']
+	. ' } ';
+
+?>
