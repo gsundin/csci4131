5beaa542c00dab0363e88361cac4a035f2b40979
Garrett Sundin <gsundin@punchthrough.com>
2017-04-27 21:55:50 -0500

    add: Functionally finished code

--- hw9/client.js
+++ hw9/client.js
@@ -1,4 +1,4 @@
-function ajax_post(){
+function ajax_post(retrieval){
     // Create our XMLHttpRequest object
     var xmlhttp = new XMLHttpRequest();
     // Create some variables we need to send to our PHP file
@@ -10,41 +10,38 @@ function ajax_post(){
     var state = document.getElementById("state").value;
     var zip = document.getElementById("zip").value;
 
-    //var previousCreds = document.getElementById("status").innerHTML;
-
-    //alert(previousCreds);
-
     var everythingFilledOut = true;
-
-    if (fn == "") {
+    if (retrieval == 0) {
+      if (fn == "") {
         alert("First name must be filled out");
         everythingFilledOut = false;
-    }
-    else if (ln == "") {
-        alert("Last name must be filled out");
-        everythingFilledOut = false;
-    }
-    else if (add1 == "") {
-        alert("Address line 1 must be filled out");
-        everythingFilledOut = false;
-    }
-    else if (city == "") {
-        alert("City must be filled out");
-        everythingFilledOut = false;
-    }
-    else if (state == "") {
-        alert("State must be filled out");
-        everythingFilledOut = false;
-    }
-    else if (zip == "") {
-        alert("Zip code must be filled out");
-        everythingFilledOut = false;
+      }
+      else if (ln == "") {
+          alert("Last name must be filled out");
+          everythingFilledOut = false;
+      }
+      else if (add1 == "") {
+          alert("Address line 1 must be filled out");
+          everythingFilledOut = false;
+      }
+      else if (city == "") {
+          alert("City must be filled out");
+          everythingFilledOut = false;
+      }
+      else if (state == "") {
+          alert("State must be filled out");
+          everythingFilledOut = false;
+      }
+      else if (zip == "") {
+          alert("Zip code must be filled out");
+          everythingFilledOut = false;
+      }
     }
 
-    var vars = "firstname="+fn+"&lastname="+ln+"&add1="+add1+"&add2="+add2+"&city="+city+"&state="+state+"&zip="+zip;
+    var vars = "firstname="+fn+"&lastname="+ln+"&add1="+add1+"&add2="+add2+"&city="+city+"&state="+state+"&zip="+zip+"&retrieval="+retrieval;
 
     if (everythingFilledOut == true) {
-      xmlhttp.open("POST", "my_parse_file.php", true);
+      xmlhttp.open("POST", "database.php", true);
     // Set content type header information for sending url encoded variables in the request
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     // Access the onreadystatechange event for the XMLHttpRequest object
--- hw9/database.php
+++ hw9/database.php
@@ -4,19 +4,38 @@ session_start();
 
 if( $_POST )
 {
-  DEFINE('DB_USER', 'gsundin');
+  $_SESSION["creds"] = '<br>{ <br> &emsp;FirstName: "' . $_POST['firstname'] . '"' . ',' . "<br>"
+    . '   &emsp;LastName: "' . $_POST['lastname'] . '"' . ',' . "<br>"
+    . '   &emsp;Address Line 1: "' . $_POST['add1'] . '"' . ',' . "<br>"
+    . '   &emsp;Address Line 2: "' . $_POST['add2'] . '"' . ',' . "<br>"
+    . '   &emsp;City: "' . $_POST['city'] . '"' . ',' . "<br>"
+    . '   &emsp;State: "' . $_POST['state'] . '"' . ',' . "<br>"
+    . '   &emsp;Zip: "' . $_POST['zip'] . '"'
+    . '<br>}';
+
+  DEFINE('DB_USER', 'root');
   DEFINE('DB_PASSWORD', 'password');  // very secure, I know
   DEFINE('DB_HOST', 'localhost');
   DEFINE('DB_NAME', 'my_database');
 
-  $conn = mysql_connect("DB_HOST","DB_USER","DB_PASSWORD");
+  $conn = mysql_connect("127.0.0.1:3306","root","password");
 
   if (!$conn)
   {
     die('Could not connect: ' . mysql_error());
   }
 
-  mysql_select_db("database", $conn);
+  $users_id = 0;
+
+  if (!$_SESSION["id"]) {
+    $_SESSION["id"] = 0;
+  } else {
+    $_SESSION["id"] = $_SESSION["id"] + 1;
+  }
+
+  $retrieval = $_POST["retrieval"];  // If TRUE, we will retrieve database instead of trying to add to it
+
+  $id_counter = $_SESSION["id"];
 
   $users_firstname = $_POST['firstname'];
   $users_lastname = $_POST['lastname'];
@@ -26,22 +45,51 @@ if( $_POST )
   $users_state = $_POST['state'];
   $users_zip = $_POST['zip'];
 
-  $users_firstname = mysql_real_escape_string($users_firstname);
-  $users_lastname = mysql_real_escape_string($users_lastname);
-  $users_address1 = mysql_real_escape_string($users_address1);
-  $users_address2 = mysql_real_escape_string($users_address2);
-  $users_city = mysql_real_escape_string($users_city);
-  $users_state = mysql_real_escape_string($users_state);
-  $users_zip = mysql_real_escape_string($users_zip);
+  mysql_select_db('my_database');
 
-  $query = "INSERT INTO `my_database`.`comments` (`firstname`, `lastname`, `add1`,
-        `add2`, `city`, `state`, `zip`) VALUES (NULL, '$users_firstname',
-        '$users_lastname', '$users_address1', '$users_address2',
-        '$users_city', '$users_state', '$users_zip');";
+  if ($retrieval == 0) {
+    $query = "INSERT INTO `addressbook` (`ID`, `First Name`, `Last Name`, `Address Line 1`,
+        `Address Line 2`, `City`, `State`, `Zip Code`) VALUES ('{$id_counter}', '{$users_firstname}',
+        '{$users_lastname}', '{$users_address1}', '{$users_address2}',
+        '{$users_city}', '{$users_state}', '{$users_zip}');";
+  } else {
+    $query = "SELECT `ID`, `First Name`, `Last Name`, `Address Line 1`,
+        `Address Line 2`, `City`, `State`, `Zip Code` FROM `addressbook`;";
+  }
 
-  mysql_query($query);
+  $retval = mysql_query($query, $conn);
 
-  echo "<h2>Thank you for your Comment!</h2>";
+
+
+  if(!$retval) {
+    die('Could not enter data: ' . mysql_error());
+  } else if ($retrieval == 0) {
+    echo "<h2>Is this a retrieval? --> " . $retrieval . "</h2><br>";
+
+    echo "<h2>Data has been submitted:</h2>";
+
+    echo $_SESSION["creds"];
+
+    echo "<br><h2>Here's what the query looked like:</h2><br>";
+
+    echo $query;
+  } else {
+    echo "<h2>Is this a retrieval? --> " . $retrieval . "</h2>";
+
+    echo "<br><h2>Data retrieved:</h2><br>";
+
+    while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
+      echo "<b>ID:</b> {$row['ID']}  <br> ".
+         "<b>First Name:</b> {$row['First Name']}  <br> ".
+         "<b>Last Name:</b> {$row['Last Name']} <br> ".
+         "<b>Address Line 1:</b> {$row['Address Line 1']} <br> ".
+         "<b>Address Line 2:</b> {$row['Address Line 2']}  <br> ".
+         "<b>City:</b> {$row['City']}  <br> ".
+         "<b>State:</b> {$row['State']}  <br> ".
+         "<b>Zip Code:</b> {$row['Zip Code']}  <br> ".
+         "--------------------------------<br>";
+    }
+  }
 
   mysql_close($conn);
 }
